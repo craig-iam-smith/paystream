8f242986fbf6fdddcf0fdfad0d9d5e914e8afa9f
Craig Smith <craig.iam.smith@gmail.com>
2022-12-08 16:23:45 -0700

    set constructor  for ERC721

--- hh/contracts/pnft.sol
+++ hh/contracts/pnft.sol
@@ -6,7 +6,9 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 
-contract repayment is Ownable {
+contract repayment is ERC721Enumerable, Ownable {
+
+    string _baseTokenURI;
 
     struct pdata {
         uint PrincipalAmount;  // total amount of principal  to be paid
@@ -20,11 +22,11 @@ contract repayment is Ownable {
     pdata Details;
 
 
-//    string _baseTokenURI;
     bool public _paused;
 
     bool public SaleStarted;
     bool public SaleEnded;
+
 
     modifier onlyWhenNotPaused {
         require (!_paused, "Contract currently paused");
@@ -34,7 +36,7 @@ contract repayment is Ownable {
     constructor (uint amount,  // total amount of principal  to be paid
         uint payments,        // number of payments to pay P&I
         uint frequency,        // how often payments are due
-        uint currency) {
+        uint currency) ERC721("PayNFT", "DIST") {
 
         Details.PrincipalAmount = amount;
         Details.NumberOfPayments = payments;
@@ -42,6 +44,7 @@ contract repayment is Ownable {
         Details.Currency = currency;
         Details.penaltySituation = false;
         Details.defaultSituation = false;
+        
     }
 
     function startSale() public onlyOwner {
